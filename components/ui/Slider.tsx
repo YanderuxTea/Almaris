878daab1379bc9a5c546850f1a7aa9e5591cc93e
Tea <teawithsuqar@mail.ru>
2025-09-16 20:23:35 +0500

    Переделанf заготовка под слайдер

--- components/ui/Slider.tsx
+++ components/ui/Slider.tsx
@@ -6,13 +6,13 @@ import Image from 'next/image'
 export default function Slider() {
   const divRef = useRef<(HTMLDivElement | null)[]>([null])
   return (
-    <section>
+    <section className='relative min-h-screen'>
       {images.map((image, i) => {
         return (
-          <div className='absolute overflow-clip z-1' key={i} ref={(el) => {
+          <div className='absolute overflow-hidden z-1 w-full h-full' key={i} ref={(el) => {
             divRef.current[i] = el
           }}>
-            <Image src={image} alt='image for slider' className='min-h-screen object-cover object-left' priority
+            <Image src={image} alt='image for slider' className='object-cover object-left brightness-75' priority
                    draggable={false}/>
           </div>)
       })}
